aa3792e185f98890c0e95b33f2f823a0721fe922
Barbara Surowiecka <bsurowie@gmail.com>
2022-07-10 18:20:05 -0400

    function description

--- min_dist_function.py
+++ min_dist_function.py
@@ -6,3 +6,14 @@ distance_graph = [[0, 7, no_path, 8],
 	             [no_path, 0, 5, no_path],
                  [no_path, no_path, 0, 2],
 	             [no_path, no_path, no_path, 0]]
+
+
+def min_dist(i, j, k, graph):
+	"""Inner recursive function finding shortest path distance between start
+	and end nodes.
+			Key arguments:
+			i -- start node
+			j -- end node
+			k -- intermediate node
+			graph -- 2D list
+			"""
