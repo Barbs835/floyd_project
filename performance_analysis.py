ca0f8420d7c24503cdf803410ab7adb1f09a32d8
Barbara Surowiecka <bsurowie@gmail.com>
2022-07-11 09:53:19 -0400

    recursive and imperative solution and graph needed to run analysis

--- performance_analysis.py
+++ performance_analysis.py
@@ -1,5 +1,15 @@
-
+import sys
 from timeit import default_timer as timer
+
+import imperative_solution
+import recursive_solution
+
+no_path = sys.maxsize
+    graph = [[0, 7, no_path, 8],
+             [no_path, 0, 5, no_path],
+             [no_path, no_path, 0, 2],
+             [no_path, no_path, no_path, 0]]
+
 
 if __name__ == '__main__':
    start = timer()
