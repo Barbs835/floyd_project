b2e86590dd5dc7815a250f9a99ab69ee119beec7
Barbara Surowiecka <bsurowie@gmail.com>
2022-07-10 21:08:51 -0400

    floyd function test

--- floyd_test.py
+++ floyd_test.py
@@ -1,9 +1,25 @@
 import unittest
+import sys
 
+from recursive_solution import floyd
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+# Testing the floyd function.
+class TestFloyd(unittest.TestCase):
+    # Define method of testing.
+    def test_floyd(self):
+        no_path = sys.maxsize
+        graph = [[0, 7, no_path, 8],
+                [no_path, 0, 5, no_path],
+                [no_path, no_path, 0, 2],
+                [no_path, no_path, no_path, 0]]
+
+        shortest_dist = [[0, 7, 12, 8],
+                         [no_path, 0, 5, 7],
+                         [no_path, no_path, 0, 2],
+                         [no_path, no_path, no_path, 0]]
+
+        result = floyd(graph)
+        self.assertEqual(result, shortest_dist)
 
 
 if __name__ == '__main__':
