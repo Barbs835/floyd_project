a8e5c6257bfb90a60b0da7d51529b0f541c418de
Barbara Surowiecka <bsurowie@gmail.com>
2022-07-10 22:02:27 -0400

    test various argumetn entries

--- floyd_test.py
+++ floyd_test.py
@@ -8,19 +8,25 @@ class TestFloyd(unittest.TestCase):
     # Define method of testing.
     def test_floyd(self):
         no_path = sys.maxsize
-        graph = [[0, 7, no_path, 8],
-                [no_path, 0, 5, no_path],
-                [no_path, no_path, 0, 2],
-                [no_path, no_path, no_path, 0]]
+        graph_1 = [[0, 7, no_path, 8],
+                   [no_path, 0, 5, no_path],
+                   [no_path, no_path, 0, 2],
+                   [no_path, no_path, no_path, 0]]
 
         shortest_dist = [[0, 7, 12, 8],
                          [no_path, 0, 5, 7],
                          [no_path, no_path, 0, 2],
                          [no_path, no_path, no_path, 0]]
 
-        result = floyd(graph)
+        result = floyd(graph_1)
         self.assertEqual(result, shortest_dist)
 
+    def test_various_arguments(self):
+        no_path = sys.maxsize
 
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(floyd([[4]]), [[0]])
+        self.assertEqual(floyd([[no_path]]), [[0]])
+        self.assertEqual(floyd([["str"]]), [[0]])
+
+    if __name__ == '__main__':
+        unittest.main()
