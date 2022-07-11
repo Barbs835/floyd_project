ffa17bde7637f0f8ea5c24f016b33da9cb914d46
Barbara Surowiecka <bsurowie@gmail.com>
2022-07-10 20:50:41 -0400

    inner function min_int

--- recursive_solution.py
+++ recursive_solution.py
@@ -14,7 +14,28 @@ def floyd(distance):
 	path = distance
 	max_length = len(distance[0])
 
+	def min_dist(i, j, k, graph):
+		"""Inner recursive function finding shortest path distance between start
+		and end nodes.
+				Key arguments:
+				i -- start node
+				j -- end node
+				k -- intermediate node
+				graph -- 2D list
+				"""
+		# If the path between i and j nodes is direct and does not go through
+		# intermediate k node, return the direct path, otherwise find the
+		# shortest distance between direct and indirecte paths.
 
+		if k == -1:
+			return(graph[i][j])
+		else:
+			return min(min_dist(i, j, k - 1, graph),
+					   min_dist(i, k, k - 1, graph)
+					   + min_dist(k, j, k - 1, graph))
+
+	# The floyd(distance) function starts by setting the start and end nodes,
+	# that will be passed to the inner min_dist() recursive function.
 	for start_node, end_node in itertools.product(range(max_length), range(max_length)):
 
 		# If start_node and end_node are the same, then the distance would be zero.
