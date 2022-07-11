09f5546c1ffb62dfaf6250c48921591f746e9444
Barbara Surowiecka <bsurowie@gmail.com>
2022-07-11 10:35:47 -0400

    time execution of floyd recursive solution

--- performance_analysis.py
+++ performance_analysis.py
@@ -5,16 +5,16 @@ import imperative_solution
 import recursive_solution
 
 no_path = sys.maxsize
-    graph = [[0, 7, no_path, 8],
-             [no_path, 0, 5, no_path],
-             [no_path, no_path, 0, 2],
-             [no_path, no_path, no_path, 0]]
+graph = [[0, 7, no_path, 8],
+         [no_path, 0, 5, no_path],
+         [no_path, no_path, 0, 2],
+         [no_path, no_path, no_path, 0]]
 
 
 if __name__ == '__main__':
    start = timer()
-   x = [r for r in range(5000)]
+   recursive_solution.floyd(graph)
    end = timer()
-   print(end - start)
-
-Result: 0.000846
+   duration = (end - start)
+   format_string = "It took %f seconds to execute Recursive solution"
+   print(format_string % duration)
